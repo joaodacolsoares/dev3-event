9c8bff282c8e1d357b0b29bf8ef3535c86d47f55
joaodacolsoares <joao.dacol.soares@gmail.com>
2022-05-22 18:43:30 -0300

    feat: add middleware to redirect when users is not logged

---
+++ pages/app/_middleware.js
@@ -0,0 +1,19 @@
+import { verify } from 'jsonwebtoken';
+import { NextResponse } from 'next/server';
+
+export async function middleware(req) {
+  if (typeof req.page.name === 'string' && !req.page.name.startsWith('/api')) {
+    try {
+      verify(req.cookies.jwt, process.env.SECRET_KEY);
+
+      return NextResponse.next();
+    } catch (e) {
+      const url = req.nextUrl.clone();
+      url.pathname = `/login`;
+      const res = NextResponse.redirect(url);
+      return res;
+    }
+  }
+
+  return NextResponse.next();
+}
