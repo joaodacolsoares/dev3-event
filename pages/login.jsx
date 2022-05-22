6f3aed671c7ee4d0010947ef1fc06da621633eed
joaodacolsoares <joao.dacol.soares@gmail.com>
2022-05-22 18:43:11 -0300

    feat: add login feature

--- pages/login.jsx
+++ pages/login.jsx
@@ -2,8 +2,11 @@ import clsx from 'clsx';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import Form from '../components/Form';
 import Template from '../components/Template';
+import useUser from '../hooks/useUser';
 
 function Card({ children, title, padding = true }) {
   return (
@@ -15,6 +18,28 @@ function Card({ children, title, padding = true }) {
 }
 
 export default function Login() {
+  const router = useRouter();
+  const { user } = useUser();
+
+  useEffect(() => {
+    if (user) {
+      router.push('/app');
+    }
+  }, [user, router]);
+
+  const onSubmit = async data => {
+    const session = await fetch('/api/login', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    }).then(res => res.json());
+
+    if (session.error) {
+      return alert(session.error);
+    }
+
+    return router.push('/app');
+  };
+
   return (
     <div className="w-full h-screen flex items-center justify-center overflow-hidden">
       <div className="flex flex-col max-w-5xl w-full">
@@ -22,11 +47,11 @@ export default function Login() {
           <div className="flex items-stretch justify-between flex-col-reverse md:flex-row md:h-auto h-screen">
             <div className="space-y-3 p-5 w-full md:flex md:justify-center md:flex-col">
               <div className="text-lg font-semibold mb-4">Entrar na conta</div>
-              <Form onSubmit={data => alert(data)}>
-                <Form.Input name="email" label="Email" />
-                <Form.Input name="password" label="Senha" />
+              <Form onSubmit={onSubmit}>
+                <Form.Email name="email" label="Email" />
+                <Form.Password name="password" label="Senha" />
                 <Form.Submit />
-                <Link href="/signup">
+                <Link href="/register">
                   <a className="text-sm text-pink-500 font-semibold text-center !mt-2">Criar conta</a>
                 </Link>
               </Form>
