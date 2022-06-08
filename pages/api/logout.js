b056ec66c231b2fd05a41a8120950c2dad4949b6
joaodacolsoares <joao.dacol.soares@gmail.com>
2022-06-08 11:57:04 -0300

    fix: remove comments from logout

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -1,5 +1,5 @@
-// const logout = async (request, response) => {
-//   return response.status(201).setHeader('Set-Cookie', ['jwt=invalid;path=/;']).send('cookie cleared');
-// };
+const logout = async (request, response) => {
+  return response.status(201).setHeader('Set-Cookie', ['jwt=invalid;path=/;']).send('cookie cleared');
+};
 
-// export default logout;
+export default logout;
