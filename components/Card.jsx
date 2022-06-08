60dc9bcc56842a3678c77b0ccefc9deaf4947681
joaodacolsoares <joao.dacol.soares@gmail.com>
2022-06-08 11:57:53 -0300

    feat(components): now Card receives classname

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,8 +1,8 @@
 import clsx from 'clsx';
 
-function Card({ children, title, padding = true }) {
+function Card({ children, className, title, padding = true }) {
   return (
-    <div className={clsx(padding && 'p-5', 'border-x-gray-100 md:rounded-lg border bg-white')}>
+    <div className={clsx(padding && 'p-5', className, 'border-x-gray-100 md:rounded-lg border bg-white')}>
       {title && <div className={clsx(!padding && 'p-5', 'font-semibold mb-3')}>{title}</div>}
       <div>{children}</div>
     </div>
